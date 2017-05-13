e41f1dfe1d91ce03435f4b78b3a58e0d7ce223ce
MukulGupta1 <mukul.gupta@gmail.com>
2017-05-13 12:44:48 +0530

    Trying to solve heroku port issue

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
     res.send('Hello world!');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Listening on port 3000...');
 });
